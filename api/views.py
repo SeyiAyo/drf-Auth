a6d3667e8e53f6e52c448a2686f0eee125b8735b
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2023-04-09 08:58:01 +0100

    update viewsets

--- api/views.py
+++ api/views.py
@@ -7,7 +7,10 @@ from rest_framework.response import Response
 from .serializers import UserSerializer,RegisterSerializer
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import generics
+from rest_framework import generics, fields, viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 
 
 # Class based view to Get User Details using Token Authentication
@@ -17,10 +20,21 @@ class UserDetailAPI(APIView):
   def get(self,request,*args,**kwargs):
     user = User.objects.get(id=request.user.id)
     serializer = UserSerializer(user)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 #Class based view to register user
-class RegisterUserAPIView(generics.CreateAPIView):
+#class RegisterUserAPIView(generics.CreateAPIView):
+class RegisterUserAPIView(viewsets.ModelViewSet):
+
+
   permission_classes = (AllowAny,)
   serializer_class = RegisterSerializer 
- 
+  
+  def create(self, request, *args, **kwargs):
+    queryset = User.objects.all()
+    serializer = self.get_serializer(data=request.data)
+    self.perform_create(serializer)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    headers = self.get_success_headers(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
